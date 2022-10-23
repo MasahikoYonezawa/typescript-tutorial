c5e522bb4907657b99774336c0a20ddf73b62420
MasahikoYonezawa <y.masahiko0802@gmail.com>
2022-10-23 22:05:31 +0900

    feat: hello world

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+if (typeof window === 'object') {
+    let message: string = "Hello, TypeScript!";
+    let heading = document.createElement("h1");
+    heading.textContent = message;
+
+    document.body.appendChild(heading)
+}
